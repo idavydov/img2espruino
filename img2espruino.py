5f5b7c924bff69e00f9806350f85b5df30fea030
Iakov Davydov <idavydov@users.noreply.github.com>
2021-11-21 10:56:14 +0100

    --invert flag

--- img2espruino.py
+++ img2espruino.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from bitarray import bitarray
 from base64 import b64encode
 
@@ -10,6 +10,7 @@ if __name__ == "__main__":
         description="Convert 1-bit image to base64 string for espruino"
     )
     parser.add_argument("--key")
+    parser.add_argument("--invert", action="store_true", help="invert image colors")
     parser.add_argument("infile")
 
     args = parser.parse_args()
@@ -17,6 +18,8 @@ if __name__ == "__main__":
     with Image.open(args.infile) as im:
         im = im.convert("1")
         arr = np.asarray(im).flatten().astype(int)
+        if args.invert:
+            arr = 1 - arr
         ba = bitarray("".join(arr.astype(str)))
         buffer = b64encode(ba.tobytes()).decode()
         if args.key:
